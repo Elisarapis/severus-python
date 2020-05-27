42f4bf1c6973e9fb5179684c8b16d1f1f40ae83a
Daniele1998 <64259267+Daniele1998@users.noreply.github.com>
2020-05-27 18:32:00 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -9,10 +9,13 @@ strike_price = 130
 volatility = 0.20
 dividend_rate = 0.0163
 option_type = ql.Option.Call
+
 risk_free_rate = 0.0001
 day_count = ql.Actual365Fixed()
 calendar = ql.Italy()
+
 calculation_date = ql.Date(24, 4, 2020)
 ql.Settings.instance().evaluationDate = calculation_date
+
 payoff = ql.PlainVanillaPayoff(option_type, strike_price)
 exercise = ql.EuropeanExercise(maturity_date)
