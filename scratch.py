868e579507a7056d7956a90281c81fdf949fc505
Daniele1998 <64259267+Daniele1998@users.noreply.github.com>
2020-05-27 18:39:54 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -26,9 +26,18 @@ kappa = 0.1
 theta = v0
 sigma = 0.1
 rho = -0.75
+
 spot_handle = ql.QuoteHandle(ql.SimpleQuote(spot_price))
 flat_ts = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, risk_free_rate, day_count))
 dividend_yield = ql.YieldTermStructureHandle(ql.FlatForward(calculation_date, dividend_rate, day_count))
 heston_process = ql.HestonProcess(flat_ts, dividend_yield, spot_handle, v0, kappa, theta, sigma, rho)
 engine = ql.AnalyticHestonEngine(ql.HestonModel(heston_process))
 european_option.setPricingEngine(engine)
+h_price = european_option.NPV()
+print(h_price)
+
+flat_vol_ts = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(calculation_date, calendar, volatility, day_count))
+bsm_process = ql.BlackScholesMertonProcess(spot_handle, dividend_yield, flat_ts, flat_vol_ts)
+european_option.setPricingEngine(ql.AnalyticEuropeanEngine(bsm_process))
+bs_price = european_option.NPV()
+print(bs_price)
