f888fbc20384b85d55d86b84fc4ccfce5149c528
colombofrancesco <64259589+colombofrancesco@users.noreply.github.com>
2020-05-27 18:24:34 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -6,3 +6,8 @@ import math
 maturity_date = ql.Date(29, 5, 2020)
 spot_price = 125
 strike_price = 130
+volatility = 0.20
+dividend_rate = 0.0163
+option_type = ql.Option.Call
+risk_free_rate = 0.0001
+day_count = ql.Actual365Fixed()
